02722fa4df6f2fa1d5fdfa2dafbbaa4faa90ee8c
Alex Kor <dna1133@gmail.com>
2024-10-29 19:27:09 +0300

    feat: added entity dto + tests

---
+++ src/simulation/interfaces/domain/entity_dto.py
@@ -0,0 +1,47 @@
+from typing import Protocol
+
+from simulation.domain.entity.base import Entity
+from simulation.domain.entity.creatures import Direction, Herbivore, Predator
+from simulation.domain.entity.landscape import Grass, Rock, Tree
+from simulation.domain.exception import ApplicationException
+
+
+_entity_dict = {
+    "Grass": Grass,
+    "Rock": Rock,
+    "Tree": Tree,
+    "Herbivore": Herbivore,
+    "Predator": Predator,
+}
+
+
+class EntityDTO:
+
+    @staticmethod
+    def entity_to_dto(entity: Entity) -> dict:
+        stats = {
+            "name": entity.name,
+            "position": (entity.x_pos, entity.y_pos),
+            "stats": {
+                "x_pos": entity.x_pos,
+                "y_pos": entity.y_pos,
+            },
+        }
+        if entity.name == "Grass":
+            stats["stats"]["health"] = entity.health
+        elif entity.name == ("Herbivore" or "Predator"):
+            stats["stats"]["health"] = entity.health
+            stats["stats"]["speed"] = entity.speed
+            stats["stats"]["target_name"] = entity.target_name
+            stats["stats"]["attack_rate"] = entity.attack_rate
+            stats["stats"]["direction"] = entity.direction.value
+        else:
+            raise ApplicationException()
+        return stats
+
+    @staticmethod
+    def dto_to_entity(dto: dict, entity_dict=_entity_dict) -> Entity:
+        entity = entity_dict[dto["name"]](**dto["stats"])
+        if entity.name == ("Herbivore" or "Predator"):
+            entity.direction = Direction(dto["stats"]["direction"])
+        return entity
---
+++ tests/interfaces/test_entity_dto.py
@@ -0,0 +1,50 @@
+from simulation.domain.entity.creatures import Direction, Herbivore
+from simulation.domain.entity.landscape import Rock
+from simulation.interfaces.domain.entity_dto import EntityDTO
+
+
+def test_herbivore_to_dto_success():
+    herbivore = Herbivore(
+        x_pos=1,
+        y_pos=2,
+        health=10,
+        target_name="Grass",
+        attack_rate=5,
+        speed=3,
+        direction=Direction.DOWN,
+    )
+    herbivore_dto = EntityDTO.entity_to_dto(herbivore)
+    assert herbivore_dto["name"] == herbivore.name
+    assert herbivore_dto["stats"]["speed"] == 3
+    assert herbivore_dto["stats"]["direction"] == (0, -1)
+
+
+def test_herbivore_from_dto_success():
+    dto = {
+        "name": "Herbivore",
+        "position": (30, 20),
+        "stats": {
+            "x_pos": 30,
+            "y_pos": 20,
+            "health": 10,
+            "attack_rate": 3,
+            "speed": 2,
+            "direction": (0, 1),
+        },
+    }
+    herbivore = EntityDTO.dto_to_entity(dto)
+    assert isinstance(herbivore, Herbivore)
+    assert herbivore.direction == Direction.UP
+
+
+def test_rock_from_dto_success():
+    dto = {
+        "name": "Rock",
+        "position": (30, 20),
+        "stats": {
+            "x_pos": 30,
+            "y_pos": 20,
+        },
+    }
+    rock = EntityDTO.dto_to_entity(dto)
+    assert isinstance(rock, Rock)
